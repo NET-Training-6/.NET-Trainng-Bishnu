fccccfa028747320e8cde6f26a58c66ab9d628a8
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-12-25 19:03:27 +0545

    Learn about arrays

--- HelloApp/EntryPoint.cs
+++ HelloApp/EntryPoint.cs
@@ -3,16 +3,19 @@ class EntryPoint
 {
     public static void Main()
     {
-        Person person1 = new();
-        person1.name = "Ram Karki";
-        person1.age = 23;
+        // Person person1 = new();
+        // person1.name = "Ram Karki";
+        // person1.age = 23;
 
-        Person person2 = new();        
-        Person person3 = new();
-        Person person4 = new();
-        Person person5 = new();
-        Person person6 = new();
-        Person person7 = new();
-        Person person8 = new();
+        // // Person person2 = new();        
+        // Person person3 = new();
+        // Person person4 = new();
+        // Person person5 = new();
+        // Person person6 = new();
+        // Person person7 = new();
+        // Person person8 = new();
+
+        ArrayLearner al = new();
+        al.LearnArrays();
     }
 }
