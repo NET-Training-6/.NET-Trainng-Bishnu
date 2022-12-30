f702e7127f28a5a7150af3f2bb66b9c5d338b52d
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-12-30 19:31:53 +0545

    More on methods

--- HelloApp/1F-Methods.cs
+++ HelloApp/1F-Methods.cs
@@ -8,7 +8,7 @@ class MethodLearner
     }
 
     // Method that returns nothing and takes some argument(s)
-    public void PrintMessage(string message)
+    public void Print(string message)
     {
         Console.WriteLine("Hello There!");
         Console.WriteLine(message);
@@ -25,19 +25,54 @@ class MethodLearner
     // and returns if number is even or odd   
     public bool IsEven(int number)
     {
-        // Implementation 1
-        // if(number % 2 == 0)
-        //     return true;
-        // else
-        //     return false;
-
-        // Implementation 2
-        // var y = number % 2 == 0 ? true : false;
-        // return y;
-
-        // Implementation 3
-        // return number % 2 == 0;
-
-        return (number & 1) == 0;
+        if(number % 2 == 0)
+            return true;
+        else
+            return false;  
     }
+
+    // Optional arguments
+    public double CalculateAverage(int num1, int num2, int num3 = 0)
+    {
+        var sum = num1 + num2 + num3;
+        var average = sum / 3;
+        return average;
+    }
+    
+    // Write a method that takes an number array and returns average of those.
+    public double CalculateAverage(params int[] numbers)   // [3, 5, 1, 8, 9]
+    {
+        var sum = 0;
+        foreach(var num in numbers)
+        {
+            sum = sum + num;
+        }
+
+        var average = sum / numbers.Length;
+        return average;
+    }
+
+    // Write a method that takes bunch of numbers and returns minimum among them.
+    public double CalculateMinimum(params int[] numbers)     // [3, 5, 1, 8, 9]
+    {
+        var min = numbers[0];
+        foreach(var num in numbers)
+        {
+            if(num < min)
+            {
+                min = num;
+            }
+        }
+
+        return min;
+    }
+
+    // Write a method to find sum and product of two numbers
+    public (int, int) GetSumAndProduct(byte n1, byte n2)    // Tuple
+    {
+        var s = n1 + n2;
+        var p = n1 * n2;
+        return (s, p);
+    }
+
 }
--- HelloApp/EntryPoint.cs
+++ HelloApp/EntryPoint.cs
@@ -21,10 +21,23 @@ class EntryPoint
         MethodLearner ml = new();
         ml.Print();
         ml.Print();
+        ml.Print("638756");
 
-        ml.PrintMessage("638756");
         var s = ml.Sum(23423.242, 4535.23);
-
         var x = ml.IsEven(314243);
+
+        // Named arguements
+        var y1 = ml.CalculateAverage(num2: 3434, num1: 345353, num3: 67868);
+        var y2 = ml.CalculateAverage(3434, 3453523);
+
+        int[] marks = { 67, 78, 98, 45, 70, 80 };
+        int[] marks1 = { 67, 78, 98, 45, 70, 80, 34, 34_534, 345_353, 3453 };
+        //var z = ml.CalculateAverage(new int[] { 67, 78, 98, 45, 70, 80 });
+        var z1 = ml.CalculateAverage(67, 78);
+        var z2 = ml.CalculateAverage(67, 78, 98, 45, 70, 80, 34, 34_534, 345_353, 3453);
+
+        var (sum, product) = ml.GetSumAndProduct(34, 56);
+        
+
     }
 }
