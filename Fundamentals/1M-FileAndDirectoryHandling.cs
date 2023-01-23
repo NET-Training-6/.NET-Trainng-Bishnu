320ae1f5989c71f3e6c697cc3418b6c6d1561781
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-01-23 18:58:35 +0545

    Learn web basics

--- Fundamentals/1M-FileAndDirectoryHandling.cs
+++ Fundamentals/1M-FileAndDirectoryHandling.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 
 class FileAndDirectory
@@ -16,7 +17,7 @@ class FileAndDirectory
         // Create a folder "A" somewhere and create a text file a.txt inside it.
         var folder1 = folderPath + "\\A";        
         Directory.CreateDirectory(folder1);
-        File.WriteAllText(folder1 + "\\a.txt", "asfashk sdfkhalkf ah");
+        File.WriteAllText(folder1 + "\\a.txt", DateTime.Now.ToString());
 
         // Create 10 folders with names Folder 1, Folder 2, Folder 3... Folder 10.
 
