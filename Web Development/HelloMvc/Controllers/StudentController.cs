10e624b6c82457ac9d92173d81369d294c2b2f93
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-02-06 07:14:18 +0545

    Add mvc template and discussion

--- Web Development/HelloMvc/Controllers/StudentController.cs
+++ Web Development/HelloMvc/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelloMvc.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelloMvc.Controllers
 {
@@ -6,12 +7,34 @@ namespace HelloMvc.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // List of students
+            Student student1 = new()
+            {
+                Name = "Ram",
+                Address = "Ktm",
+                Email = "ram@gmail.com",
+                Dob = new DateTime(1998, 8, 12)
+            };
+
+            Student student2 = new()
+            {
+                Name = "Shyam Basnet",
+                Address = "Bhaktapur",
+                Email = "shyam@gmail.com",
+                Gender = Sex.Male,
+                Dob = new DateTime(1995, 3, 24)
+            };
+
+            List<Student> students = new() { student1, student2 };
+
+            return View(students);
         }
 
         public IActionResult Test()
         {
-            return View();
+            int[] numbers = { 2, 3, 4, 5, 6, 12, 78, 98, 12, 34, 54, 32, 21 };
+            return View(numbers);
         }
     }
+    
 }
--- Web Development/HelloMvc/Models/Student.cs
+++ Web Development/HelloMvc/Models/Student.cs
@@ -2,5 +2,16 @@
 
 public class Student
 {
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public DateTime Dob { get; set; }
+    public Sex Gender { get; set; } = Sex.Male;
+}
 
+public enum Sex
+{
+    Male = 1,
+    Female = 2,
+    Others = 3
 }
