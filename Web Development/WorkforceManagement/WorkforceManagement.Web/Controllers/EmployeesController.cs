a352d225f766c3bceea5dfacf1cca7765b8dee5d
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-02-06 07:14:42 +0545

    Add HRM project template and few changes

---
+++ Web Development/WorkforceManagement/WorkforceManagement.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkforceManagement.Web.Enums;
+using WorkforceManagement.Web.Models;
+
+namespace WorkforceManagement.Web.Controllers;
+public class EmployeesController : Controller
+{
+    public IActionResult Index()
+    {
+        Employee employee1 = new Employee
+        {
+            Name = "Bishnu Rawal",
+            Gender = Gender.Male,
+            Contact = "7983798789",
+            Address = "Kathmandu",
+            Dob = new DateTime(1988, 12, 23),
+            Department = "Research & Development",
+            Designation = "Software Architect"
+        };
+
+        Employee employee2 = new Employee
+        {
+            Name = "Bishnu Rawal",
+            Gender = Gender.Male,
+            Contact = "7983798789",
+            Address = "Kathmandu",
+            Dob = new DateTime(1988, 12, 23),
+            Department = "Research & Development",
+            Designation = "Software Architect"
+        };
+
+        List<Employee> employees = new() { employee1, employee2 };
+
+        return View(employees);
+    }
+}
---
+++ Web Development/WorkforceManagement/WorkforceManagement.Web/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using WorkforceManagement.Web.Enums;
+
+namespace WorkforceManagement.Web.Models;
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public Gender Gender { get; set; }
+    public DateTime Dob { get; set; }
+    public string Contact { get; set; } = string.Empty;
+    public DateTime JoinDate { get; set; }
+    public string Department { get; set; } = string.Empty;
+    public string Designation { get; set; } = string.Empty;
+}
