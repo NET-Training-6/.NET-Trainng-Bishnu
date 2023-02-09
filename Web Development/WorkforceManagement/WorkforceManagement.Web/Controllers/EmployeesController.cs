33ad7f9f9af9c3bbc5aac35532df14ee1041d026
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-02-09 18:57:50 +0545

    Learn ADO.NET

--- Web Development/WorkforceManagement/WorkforceManagement.Web/Controllers/EmployeesController.cs
+++ Web Development/WorkforceManagement/WorkforceManagement.Web/Controllers/EmployeesController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using WorkforceManagement.Web.Enums;
 using WorkforceManagement.Web.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WorkforceManagement.Web.Controllers;
 public class EmployeesController : Controller
@@ -31,6 +35,45 @@ public class EmployeesController : Controller
 
         List<Employee> employees = new() { employee1, employee2 };
 
+        return View(employees);
+    }
+
+    public IActionResult GetData()
+    {
+        List<Employee> employees = new List<Employee>();
+
+        string connectionString = 
+            @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=Workforce_Test;Integrated Security=true;";
+
+        string queryString = "SELECT * from dbo.Employee";
+        
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(queryString, connection);
+        
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var employee = new Employee()
+                    {
+                        Name =  reader.GetString(1),
+                        Dob =  reader.GetDateTime(3),
+                        //Gender = reader.GetFieldValue<Gender>(2),
+                        Contact = reader.GetString(4)
+                    };
+
+                    employees.Add(employee);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         return View(employees);
     }
 }
