f5acb3802ef49e5300707c2caac46aeacbb17b89
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-02-13 19:46:29 +0545

    Add client side validations

--- Web Development/WorkforceManagement/WorkforceManagement.Web/Models/Employee.cs
+++ Web Development/WorkforceManagement/WorkforceManagement.Web/Models/Employee.cs
@@ -1,15 +1,29 @@
-﻿using WorkforceManagement.Web.Enums;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using WorkforceManagement.Web.Enums;
 
 namespace WorkforceManagement.Web.Models;
 public class Employee
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Enter your name")]
+    [MinLength(2, ErrorMessage = "At least 2 letters please")]
     public string Name { get; set; } = string.Empty;
+
     public string? Address { get; set; } = string.Empty;
     public Gender Gender { get; set; }
+    
+    [DisplayName("Date of birth")]
+    [DataType(DataType.Date)]
     public DateTime Dob { get; set; }
+    
     public string Contact { get; set; } = string.Empty;
+    
+    [DisplayName("Join Date")]
+    [DataType(DataType.Date)]
     public DateTime JoinDate { get; set; }
+    
     public string Department { get; set; } = string.Empty;
     public string Designation { get; set; } = string.Empty;
 }
